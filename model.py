73fc1885a702493e23c88cd03e05a8b06e7a034c
Houssem Jebari <houssemjebari98@gmail.com>
2024-12-10 15:14:24 -0500

    main: Implements the forward pass method for the whole model.

--- model.py
+++ model.py
@@ -83,3 +83,19 @@ class GPT(nn.Module):
             ln_f = nn.LayerNorm(config.n_embd),
         ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+    def forward(self, x, targets=None):
+        B, T = x.size() # (B,T)
+        assert T <= self.config.block_size, "The input sequence shouldn't be longer than {self.config.block_size} words"
+        positions = torch.arange(0, T, dtype=torch.long) # (T,)
+        x = self.transformer.wte(x) + self.transformer.wpe(positions) # (B,T,n_embd) + (T,n_embd)
+        for block in self.transformer.h:
+            x = block(x)
+
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x)
+        print('logits shape: ', logits.shape)
+        loss = None
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1,logits.size(-1)),targets.view(-1))
+        return x, loss
