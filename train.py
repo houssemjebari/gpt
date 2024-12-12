c53ba5b97671224088bf293162132b72efadaa37
Houssem Jebari <houssemjebari98@gmail.com>
2024-12-12 13:12:03 -0500

    main: Implements a base training loop

---
+++ train.py
@@ -0,0 +1,39 @@
+from  model import GPT, GPTConfig
+from data_loader import DataLoader
+import torch
+import time
+
+# detect the device to use
+device = "cpu"
+torch.manual_seed(1337)
+if torch.cuda.is_available():
+    device = 'cuda'
+    torch.cuda.manual_seed(1337)
+print(f'using device: {device}')
+
+# Import the data loader 
+batch_size = 4
+context_size = 1024
+train_loader = DataLoader(batch_size,context_size)
+
+# Instantiate the Model
+model = GPT(GPTConfig)
+model.to(device)
+
+# Optimize 
+train_steps = 50
+optimizer = torch.optim.AdamW(model.parameters(),lr =3e-4)
+for i in range(train_steps):
+    t0 = time.time()
+    x,y = train_loader.next_batch()
+    x, y = x.to(device), y.to(device)
+    optimizer.zero_grad()
+    logits, loss = model(x,y)
+    loss.backward()
+    optimizer.step()
+    torch.cuda.synchronize()
+    step_time = time.time() - t0
+    tokens_per_sec = (batch_size * context_size) / step_time
+    print(f'step {i}, loss: {loss.item()} dt: {step_time:.2f} ms tok/sec: {tokens_per_sec}')
+
+
