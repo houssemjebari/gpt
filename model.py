04c11bfd9284aa0e74760c2183ea041a857966cf
Houssem Jebari <houssemjebari98@gmail.com>
2024-12-15 13:33:36 -0500

    main: Changes the vocab size to a nice number by default

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ import tiktoken
 @dataclass 
 class GPTConfig:
     block_size: int = 1024 # max sequence length 
-    vocab_size: int = 50257 # number of tokens: 50k BPE Merges
+    vocab_size: int = 50304 # number of tokens: 50k BPE Merges
     n_layer:  int = 12 # number of attention block layers
     n_head: int = 12 # number of heads 
     n_embd: int = 768 # Embedding
